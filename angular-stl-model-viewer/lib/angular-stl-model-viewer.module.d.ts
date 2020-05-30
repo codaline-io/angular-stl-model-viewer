cc820bed087b82aa19a6bd8e1026f71912db7391
Bengt Weiße <bengtler@gmail.com>
2020-05-30 21:21:32 +0200

    Updates

--- angular-stl-model-viewer/lib/angular-stl-model-viewer.module.d.ts
+++
@@ -1,8 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './angular-stl-model-viewer.component';
-export declare class StlModelViewerModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StlModelViewerModule, [typeof ɵngcc1.StlModelViewerComponent], never, [typeof ɵngcc1.StlModelViewerComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<StlModelViewerModule>;
-}
-
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5ndWxhci1zdGwtbW9kZWwtdmlld2VyLm1vZHVsZS5kLnRzIiwic291cmNlcyI6WyJhbmd1bGFyLXN0bC1tb2RlbC12aWV3ZXIubW9kdWxlLmQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTs7O0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVjbGFyZSBjbGFzcyBTdGxNb2RlbFZpZXdlck1vZHVsZSB7XG59XG4iXX0=
--- angular-stl-model-viewer/public-api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/angular-stl-model-viewer.component';
-export * from './lib/angular-stl-model-viewer.module';
