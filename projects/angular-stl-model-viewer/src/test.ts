dc61b901a3f2a19f174028ffcad6cba5b60d9b4d
Bengt Weiße <bengtler@gmail.com>
2022-11-21 11:34:03 +0100

    angular 15

--- projects/angular-stl-model-viewer/src/test.ts
+++ projects/angular-stl-model-viewer/src/test.ts
@@ -20,7 +20,3 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 )
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/)
-// And load the modules.
-context.keys().map(context)
